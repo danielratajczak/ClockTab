3eecdc45fbb90fbd9dd44ca43ac28b36799d5eb7
Daniel <daniel.ratajczak.1@student.put.poznan.pl>
2021-01-16 17:03:54 +0100

    Add initial files

---
+++ index.js
@@ -0,0 +1,22 @@
+function GetClock() {
+  var d = new Date();
+  var nmonth = (d.getMonth() + 1),
+  ndate = d.getDate(),
+  nyear = d.getFullYear();
+  if (nmonth <= 9) nmonth = "0" + nmonth;
+  if (ndate <= 9) ndate = "0" + ndate;
+  var nhour = d.getHours(),
+    nmin = d.getMinutes(),
+    nsec = d.getSeconds();
+  if (nmin <= 9) nmin = "0" + nmin;
+  if (nsec <= 9) nsec = "0" + nsec;
+
+  var clocktext = nhour + ":" + nmin + ":" + nsec + "";
+  document.getElementById('clockbox').innerHTML = clocktext;  
+}
+
+
+window.onload = function () {
+  GetClock();
+  setInterval(GetClock, 1000);
+}
